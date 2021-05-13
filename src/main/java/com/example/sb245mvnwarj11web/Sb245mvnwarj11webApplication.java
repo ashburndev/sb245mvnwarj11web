560020a790ae9297a506d270cce663082516c760
ashburndev <ashburndev@gmail.com>
2021-05-13 07:36:02 -0400

    first commit

---
+++ src/main/java/com/example/sb245mvnwarj11web/Sb245mvnwarj11webApplication.java
@@ -0,0 +1,13 @@
+package com.example.sb245mvnwarj11web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb245mvnwarj11webApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb245mvnwarj11webApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sb245mvnwarj11web/Sb245mvnwarj11webApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sb245mvnwarj11web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb245mvnwarj11webApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
